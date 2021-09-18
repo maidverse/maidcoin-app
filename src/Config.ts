ae09580f50661e048b25c1196683702d2ef7dff2
Dr. Ada <contact@maidcoin.org>
2021-09-19 02:26:39 +0900

    deploy to ropsten

--- src/Config.ts
+++ src/Config.ts
@@ -8,36 +8,36 @@ export default {
     //chainId: 1,
     //blockTimeSecond: 15,
 
-    // Kovan
-    chainId: 42,
-    blockTimeSecond: 4,
+    // Ropsten
+    chainId: 3,
+    blockTimeSecond: 30,
 
     infuraId: INFURA_ID,
 
     // Mainnet
     //endpoint: `wss://mainnet.infura.io/ws/v3/${INFURA_ID}`,
 
-    // Kovan
-    endpoint: `wss://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+    // Ropsten
+    endpoint: `wss://ropsten.infura.io/ws/v3/${INFURA_ID}`,
 
     contracts: {
 
         // Mainnet
 
-        // Kovan
-        LPToken: "0x56ac87553c4dBcd877cA7E4fba54959f091CaEdE",
-        Sushi: "0xcd280c22F70d6f58B34a1cCbd41780979BBC2F3B",
+        // Ropsten
+        LPToken: "0xF43df1bC8DD096F5f9dF1fB4d676D2ab38592020",
+        Sushi: "0x81db9c598b3ebbdc92426422fc0a1d06e77195ec",
 
-        MaidCoin: "0xc3a7DC0D11909503aa0Ef1c7368B4EC7ac2e6E5C",
-        MaidCafe: "0x40f060711b3250559762A70EdBBcc3C79Ff80Aa9",
-        TheMaster: "0x25312764c5a74992b87Bf383981B6563e2090EC8",
-        Maids: "0x43B1D98478F355ddb0A2c44b89EB4e886C8327f9",
-        MasterCoin: "0xfcd6c88109c487a1644aDCBE9F080210606eFAA7",
-        NursePart: "0xC3B6eeb2c635126D211FfE7F6598b3808c36BdfA",
-        NurseRaid: "0xf72ccEFE49d2E3F966bEacd2bC38617Cc4911518",
-        CloneNurses: "0xc6FE9c4A0497d993B9b52c3024D9be466A993e1b",
+        MaidCoin: "0x4a13A8a1646029B9d08e6b2a1C5b1f97b78926cf",
+        MaidCafe: "0xAe080415d34155eE6CfF37e1a5dFea997E3Acb1c",
+        TheMaster: "0x10b8Fe9248b771eEBE46ea9eBAD7A4D59be714cE",
+        Maids: "0x449E9AcEDC00dE3De74C547a130C76Aa62d75cf2",
+        MasterCoin: "0x28212f2D8987fF7590eaC26384c1cE93C4F8071E",
+        NursePart: "0x664Bf421C9ce702905202972145F881DB4b140ad",
+        CloneNurses: "0xa59EE8162C7195DB34A81b8bee679654fDc3c37C",
+        NurseRaid: "0xaCA9C5e4E2E2b8f34AC8EcafAb7F2990ec02C0dd",
 
-        SushiGirls: "0xd48ec06ee1e1016c159415262A2dFa233E325C2F",
-        LingerieGirls: "0xB555cA9C88CeB8ece57b9223AA6D7407dD273656",
+        SushiGirls: "0x9fF326fecc05A5560Eea1A66C6c62a93a64afaFb",
+        LingerieGirls: "0xf35f860762540929B3157765B82E6616664f7e97",
     },
 };
