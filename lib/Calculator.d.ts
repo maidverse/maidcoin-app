f533db590d35c8e52640050725b4564cc35d860a
Dr. Ada <contact@maidcoin.org>
2021-09-25 00:58:30 +0900

    deploy to mainnet

--- lib/Calculator.d.ts
+++ lib/Calculator.d.ts
@@ -1,6 +1,6 @@
 declare class Calculator {
     nurseLifetime(nurseType: number, partCount: number, assemble: boolean): number;
-    apr(poolId: number): Promise<import("ethers").BigNumber | 0>;
+    apr(poolId: number): Promise<0 | import("ethers").BigNumber>;
 }
 declare const _default: Calculator;
 export default _default;
