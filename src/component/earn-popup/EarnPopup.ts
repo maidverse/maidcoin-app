d9107a114042aafd2f2b245b8bc76dea36fcf339
Dr. Ada <contact@maidcoin.org>
2021-10-07 00:35:10 +0900

    add nurse types

---
+++ src/component/earn-popup/EarnPopup.ts
@@ -0,0 +1,26 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { DomNode, el, Popup } from "@hanul/skynode";
+import { utils } from "ethers";
+import CommonUtil from "../../CommonUtil";
+
+export default class EarnPopup extends Popup {
+
+    public content: DomNode;
+
+    constructor(amount: BigNumber, apr: BigNumber) {
+        super(".popup-background");
+        this.append(
+            this.content = el(".earn-popup",
+                el(".amount", `+ ${CommonUtil.displayPrice(amount)}`),
+                el(".apr", "APR: ", el("span", `${CommonUtil.numberWithCommas(apr.toString())}%`)),
+                el("a.close-button", el("img", { src: "/images/component/first-earn-popup/close-button.png", height: "20" }), {
+                    click: () => this.delete(),
+                }),
+                el("a.tweet-button",
+                    "Share",
+                    { href: `https://twitter.com/intent/tweet?text=${encodeURIComponent(`I earned ${CommonUtil.displayPrice(amount)} $MAID !\nhttps://app.maidcoin.org`)}`, target: "_blank" },
+                ),
+            ),
+        );
+    }
+}
