ff57f348f784fb7e1ae99cb45de05e00b11355cf
Dr. Ada <contact@maidcoin.org>
2021-12-05 15:59:00 +0900

    update

--- lib/Calculator.d.ts
+++ lib/Calculator.d.ts
@@ -1,10 +1,10 @@
 import { BigNumber } from "ethers";
 declare class Calculator {
     nurseLifetime(nurseType: number, partCount: number, assemble: boolean): number;
-    poolAPR(poolId: number): Promise<BigNumber>;
+    poolAPR(poolId: number): Promise<number>;
     dollar(amount: BigNumber): Promise<BigNumber>;
-    cafeAPR24(): Promise<BigNumber>;
-    nurseAPR(_nurseType: number): Promise<BigNumber>;
+    cafeAPR24(): Promise<number>;
+    nurseAPR(_nurseType: number): Promise<number>;
 }
 declare const _default: Calculator;
 export default _default;
